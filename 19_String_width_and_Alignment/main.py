4c5d9d16874e5262f71b1f631f393aa57eb1bb32
aliasghor <aliasghor6@gmail.com>
2023-12-20 21:51:31 +0800

    Commiting folder part 19 on my master branch

--- 19_String_width_and_Alignment/main.py
+++ 19_String_width_and_Alignment/main.py
@@ -1,5 +1,34 @@
-balikArray = lambda array : array[::-1]
+class Planet:
+    def __init__(self, planetName, naturalSatellites):
+        self.__planetName = planetName
+        self.__naturalSatellites = naturalSatellites
 
-list = [1,2,3,4,5]
-print(f"List before get reverse = {list}")
-print(f"List after got reversed = {balikArray(list)}")
+    def getPlanetName(self):
+        return self.__planetName
+    
+    def getNaturalSatellites(self):
+        return self.__naturalSatellites 
+    
+    def __str__(self):
+        return f"{self.__planetName} has a natural satellite called {self.__naturalSatellites}"
+    
+
+earth = Planet("Earth","Moon")
+
+# Mengambil attributs private melalui methods
+data = f"""Nama planet: {earth.getPlanetName()}
+Nama satellite: {earth.getNaturalSatellites()}
+"""
+print(data)
+
+# Mengatur lebar
+
+data = f"""
+Nama planet: {earth.getPlanetName(): ^17}
+Nama satellite: {earth.getNaturalSatellites(): ^10}
+"""
+print(data)
+
+
+print(earth)
+
