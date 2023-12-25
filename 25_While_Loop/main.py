40d37c1a05f10ec7a5c87222ea355fb6f8898719
aliasghor <aliasghor6@gmail.com>
2023-12-25 21:10:59 +0800

    Commiting again my folder part 25

--- 25_While_Loop/main.py
+++ 25_While_Loop/main.py
@@ -1,5 +1,18 @@
-angka = 0
+"""
+    contoh operator assignment
+    i = 0 <- membuat variabel i dimulai dari 0
+    i = i + 1 <- assign ulang nilai variabel i lalu didalam variabel i ane tambahkan dengan i + 1
+    jadi logikanya adalah
+    
+    i = 0
+    i = i + 1 <- ini namanya meng assign ulang sebuah nilai.dengan menambahkan nilai 0 + 1 = 1
+"""
 
-while angka < 5:
-    angka += 1
-    print(angka)
+i = 0 # <- i adalah sebuah "Konvensi" konvensi adalah sebuah penamaan untuk standarisasi looping
+
+while i < 5: # <- Disini ane membikin sebuah kondisi.kondisinya adalah apakah i < 5? Jawabannya True dan akan melakukan looping
+    print(f"i sekarang -> {i}") # <- Print i setiap kali berada dalam kondisi True yaitu i < 5
+    i += 1 # <- Ini namanya operator assignment kalo gk salah hehe..fungsi operator disini supaya untuk membikin kondisi menjadi False.jadi setiap looping yang terjadi dalam scope ini akan ditambahkan dengan 1
+
+print("Program Selesai")
+
