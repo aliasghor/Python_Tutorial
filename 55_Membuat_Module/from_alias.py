f2709108a03e2fb6547a577a8bb6b4226b765f30
aliasghor <aliasghor6@gmail.com>
2024-01-03 22:37:42 +0800

    Commiting my folder part 55

---
+++ 55_Membuat_Module/from_alias.py
@@ -0,0 +1,14 @@
+import matematika as math
+# from matematika import tambah as t
+# from matematika import kali as k
+# from matematika import pangkat as p
+
+
+hasil_tambah = math.tambah(4,5)
+print(f"Hasil fungsi dari module tambah = {hasil_tambah}")
+
+hasil_kali = math.kali(4,5)
+print(f"Hasil fungsi dari module kali = {hasil_kali}")
+
+hasil_pangkat = math.pangkat(4,5)
+print(f"Hasil fungsi dari module pangkat = {hasil_pangkat:,}")
---
+++ 55_Membuat_Module/from_main.py
@@ -0,0 +1,10 @@
+from matematika import *
+
+hasil_tambah = tambah(4,5)
+print(f"Hasil fungsi dari module tambah = {hasil_tambah}")
+
+hasil_kali = kali(4,5)
+print(f"Hasil fungsi dari module kali = {hasil_kali}")
+
+hasil_pangkat = pangkat(4,5)
+print(f"Hasil fungsi dari module pangkat = {hasil_pangkat:,}")
