51c257560c207f5467ace53be2f2697cbbbdb44e
aliasghor <aliasghor6@gmail.com>
2024-01-13 14:50:53 +0700

    Commit my Python basics folder part 43b_Latihan_Dictionary_Part2 again because i want to revised my old codes

--- 43b_Latihan_Dictionary_Part2/main.py
+++ 43b_Latihan_Dictionary_Part2/main.py
@@ -1,51 +1,59 @@
 import datetime as dt
 import os
-import random
 import string
+import random
 
 data_template = {
-    "NAME":"UNKOWN",
-    "AGE":"UNKOWN",
-    "STUDENT-ID":"UNKNOWN",
-    "BORNED":"UNKOWN"
+    "Name":"Unknown",
+    "Age":"Unknown",
+    "Major":"Unknown",
+    "Student-Id":"Unknown",
+    "Borned":"Unknown"
 }
 
 students_data = {}
 
 while True:
     os.system("cls")
-    print("Welcome to student database")
-    print("Please fill your personal data",end='\n\n')
+    print(f"{'Welcome to students database program':^50}")
+    print(f"{'Please fill your personal data below':^50}")
+    print(50 * "-",end='\n\n')
 
     students = dict.fromkeys(data_template.keys())
 
-    students['NAME'] = input("Please input your name: ")
-    students['AGE'] = int(input("Please input your age: "))
-    students['STUDENT-ID'] = input("Please input your students id: ")
-    year = int(input("Please input your borned year (YYYY): "))
-    month = int(input("Please input your borned month (1-12): "))
-    date = int(input("Please input your borned date (1-31): "))
-    students['BORNED'] = dt.date(year,month,date)
+    try:
+        students['Name'] = input("Please input your students name: ")
+        students['Age'] = int(input("Please input your students age: "))
+        students['Major'] = input("Please input your major: ")
+        students['Student-Id'] = input("Please input your student id: ")
 
-    KEY = ''.join((random.choice(string.ascii_uppercase) for i in range(6)))
+        YEAR = int(input("Please input your borned year (YYYY): "))     
+        MONTH = int(input("Please input your borned month (1-12): "))
+        DATE = int(input("Please input your borned date (1-31): "))
 
-    students_data.update({KEY:students})
+        students['Borned'] = dt.date(YEAR,MONTH,DATE)
 
-    print(f"{'KEY':^20} | {'NAME':^20} | {'AGE':^20} | {'STUDENT-ID':^20} | {'BORNED(year,month,date)':^20} | {'BORNED(DAY)'}")
+        KEY = ''.join((random.choice(string.ascii_uppercase) for i in range(6)))
+        students_data.update({KEY:students})
 
-    for student in students_data:
-        key = student
-        NAME = students_data[key]['NAME']
-        AGE = students_data[key]['AGE']
-        STUDENT_ID = students_data[key]['STUDENT-ID']
-        BORNED = students_data[key]['BORNED'].strftime("%x")
-        BORNED_DAY = students_data[key]['BORNED']
+        print(f"{'KEY':^20} {'NAME':^20} {'AGE':^20} {'MAJOR':^20} {'STUDENT_ID':^20} {'BORNED':^20}")
+        print(130 * '-')
+        for students in students_data:
+            KEY = students
+            NAME = students_data[KEY]['Name']
+            AGE = students_data[KEY]['Age']
+            MAJOR = students_data[KEY]['Major']
+            STUDENT_ID = students_data[KEY]['Student-Id']
+            BORNED = students_data[KEY]['Borned'].strftime("%x")
 
-        print(f"{key:^20} | {NAME:^20} | {AGE:^20} | {STUDENT_ID:^20} | {BORNED:^20}    | {BORNED_DAY:%A}")
+            print(f"{KEY:^20} {NAME:^20} {AGE:^20} {MAJOR:^20} {STUDENT_ID:^20} {BORNED:^20}")
+
+    except:
+        print("An error occured on the program.please try again")
 
     is_done = input("Do you want to exit from this program? (Y/N): ")
+
     if is_done == 'y' or is_done == 'Y':
-        os.system("cls")
         break
 
-print("Program Finished")
+print("Program students database finished")
