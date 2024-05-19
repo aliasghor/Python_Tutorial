c670f5d428eee4916e3a461c02d680eec202f962
aliasghor <aliasghor6@gmail.com>
2024-05-19 21:29:50 +0700

    Made a few changes, added an explaination what Inheritance is on the directory 12_Pendahuluan_Inheritance.

--- Python_OOP/From_Kelas_Terbuka/12_Pendahuluan_Inheritance/main.py
+++ Python_OOP/From_Kelas_Terbuka/12_Pendahuluan_Inheritance/main.py
@@ -1,22 +1,27 @@
+"""
+    Inheritance is a concept in Object-Oriented Programming (OOP) in which a based class inherits/derives attributes, methods to the derived class; without need to implement or rewrite it all over again from the begining.
+"""
+
 class Hero:
-    def __init__(self, name: str, health: int) -> None:
+    def __init__(self, name: str, health: int, armor: int) -> None:
         self.name = name
         self.health = health
+        self.armor = armor
 
-    def __str__(self):
-        return f"Hero's name: {self.name}; has a health for about: {self.health}."
-
-class HeroIntelligent(Hero):
-    pass
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}('{self.name}', {self.health}, {self.armor})"
 
 class HeroStrength(Hero):
     pass
 
-gerry = Hero("Gerry",100)
-mogi = HeroIntelligent("Mogi",50)
-sampi = HeroStrength("Sampi",30)
+class HeroIntelligent(Hero):
+    pass
+
+gerry = HeroStrength("Gerry",100,40)
+mogi = HeroIntelligent("Mogi",150,20)
+
 print(gerry)
 print(mogi)
-print(sampi)
 
+print(help(gerry))
 print(help(mogi))
